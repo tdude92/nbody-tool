95509bccdf0a51ce3e00840427c100f31540524d
tdude <trevorguanhao@gmail.com>
2021-10-25 12:28:53 -0400

    link glad.c to project

--- demo/nbt_demo_2d.cpp
+++ demo/nbt_demo_2d.cpp
@@ -1,7 +1,54 @@
 #include <iostream>
 #include <fstream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <Eigen>
 
-int main(int argc, char* argv[]) {
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+int main() {
+    // Initialize GLFW context
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Create window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Galaxy Garden v2", NULL, NULL);
+    if (window == NULL) {
+        std::cerr << "Failed to create GLFW window." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD." << std::endl;
+        return -1;
+    }
+
+    // OpenGL config
+    glViewport(0, 0, 800, 600);
+
+    // Set window callbacks
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+    // Render loop
+    while (!glfwWindowShouldClose(window)) {
+        // Render
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Process events and swap buffers
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    } 
+
+    glfwTerminate();
     return 0;
 }
